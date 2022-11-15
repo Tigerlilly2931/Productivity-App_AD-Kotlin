560a1ee7e554151e633ec9f877caac27d2a008e7
Tigerlilly2931 <55060114+Tigerlilly2931@users.noreply.github.com>
2022-11-15 01:13:01 -0600

    Cant make checklist clear or update... No understandy

--- app/src/main/java/com/example/productivity_app_ad/Checklist.kt
+++ app/src/main/java/com/example/productivity_app_ad/Checklist.kt
@@ -4,9 +4,15 @@ import java.util.*
 
 class Checklist {
     var messageText: String? = null
+    var messageUser: String? = null
+    var completedCheck: Boolean? = false
 
-    constructor(messagetext: String?) {
+    constructor(messagetext: String?, messageuser: String?) {
         this.messageText = messagetext
+        this.messageUser = messageuser
+    }
+    constructor(isDone: Boolean?){
+        completedCheck = isDone
     }
 
     constructor(){
--- app/src/main/java/com/example/productivity_app_ad/MainActivity.kt
+++ app/src/main/java/com/example/productivity_app_ad/MainActivity.kt
@@ -1,36 +1,48 @@
 package com.example.productivity_app_ad
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.format.DateFormat
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.EditText
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.database.FirebaseListAdapter
 import com.firebase.ui.database.FirebaseListOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
+
 
 class MainActivity : AppCompatActivity() {
     private var adapter: FirebaseListAdapter<Checklist>? = null
     private var options: FirebaseListOptions<Checklist>? = null
+
+    //private val checkBoxList: MutableList<String>? = null
+
     private val stuffs: TextView? = null
     private var fabb: FloatingActionButton? = null
     private var currentUser: FirebaseUser? = null
     private var mAuth: FirebaseAuth? = null
     private var userName: String? = null
+    private var userNameHolder: TextView? = null
+    //private var checkboxThing: CheckBox? = null
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mAuth = FirebaseAuth.getInstance()
         currentUser = mAuth!!.currentUser
         userName = mAuth!!.currentUser!!.displayName
+        userNameHolder = findViewById(R.id.UsernameHolder)
+        //checkboxThing = findViewById(R.id.chechbox)
+        var strHold: String? = "Welcome $userName"
+        userNameHolder?.text = strHold
         if (currentUser == null) {
             val intent = Intent(this@MainActivity, RegisterActivity::class.java)
             startActivity(intent)
@@ -46,7 +58,7 @@ class MainActivity : AppCompatActivity() {
             FirebaseDatabase.getInstance()
                 .reference
                 .push()
-                .setValue(Checklist(input.text.toString())
+                .setValue(Checklist(input.text.toString(), userName)
                 )
 
             // Clear the input
@@ -61,14 +73,37 @@ class MainActivity : AppCompatActivity() {
             override fun populateView(v: View, model: Checklist, position: Int) {
                 // Get references to the views of message.xml
                 val messageText = v.findViewById<View>(R.id.message_text) as TextView
+                val checkBoxWahh = v.findViewById<CheckBox>(R.id.chechbox) as CheckBox
+                checkBoxWahh.setOnCheckedChangeListener { button, b ->
 
-                // Set their text
-                messageText.text = model.messageText
+
+                    //doing remove value here clears the entire view...
+                    FirebaseDatabase.getInstance().reference.removeValue()
+
+                    Toast.makeText(this@MainActivity,b.toString(),Toast.LENGTH_SHORT).show()
+                }
+                if(userName.equals(model.messageUser)){
+                    // Set their text
+                    messageText.text = model.messageText
+                }else{
+                    //don't display the checklist for user
+
+                }
+
 
             }
         }
         listOfMessages.adapter = adapter
     }
+
+    fun onCheckboxClicked(view: View){
+        if(view is CheckBox){
+            val checked: Boolean = view.isChecked
+
+        }
+    }
+
+
 
     override fun onStart() {
         super.onStart()
